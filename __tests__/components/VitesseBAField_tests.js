d8060742e8114ff9b089f693074340c4586cedcd
UNIVERSE\eklimenko <Eugene.Klimenko@dataart.com>
2017-05-09 22:14:57 +0300

    components tests work

---
+++ __tests__/components/VitesseBAField_tests.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import VitesseBAField from '../../src/VitesseBAField'
+
+function setup() {
+  const props = {
+    pars: {
+      Field: 'Field1',
+      FieldName: 'FieldName1'
+    }
+  }
+
+  const vitesseBAField = shallow( <VitesseBAField {...props}/>)
+
+  return {
+    props,
+    vitesseBAField
+  }
+}
+
+describe('VitesseBAField component', () => {
+  it('should render self and subcomponents', () => {
+    const {props, vitesseBAField} = setup()
+    let fieldNameDiv = vitesseBAField.find('div')//.findWhere(d => d.text() == props.pars.FieldName);
+    console.log(vitesseBAField.html());
+    console.log(fieldNameDiv.text());
+    expect(fieldNameDiv.text()).toBe(props.pars.FieldName + ':');
+  });
+});
+
